67c605c528bb03160937c3b12dffd6e23ee0d874
Sergio Valentín Asenjo Asencio <sergio.asenjo@ibm.com>
2022-05-23 16:30:58 -0400

    build(web): añade ambientes de des, qa y prod.
    Se añaden las variables de ambiente correspondiente para desarrollo,
    qa y producción.
    
    Se añaden comandos para iniciar directo sobre un ambiente:
    dev -> npm run dev
    qa -> npm run qa
    prod -> npm run prod
    
    Se descomenta función de histórico de sesiones que tenía dependencia
    en los ambientes para qa y prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  urlbase: '',
+  URL_BFF_BASE: 'https://vs-as-appsalud-back-prod.azurewebsites.net',
+  X_IBM_CLIENT_ID: '', /* REVISAR CUÁL CORRESPONDE A PROD */
+  CLIENT_ID: 'vs-web-salud',
   URL_SSO: 'https://sso.security.cl/auth/realms/VS-Clients/protocol/openid-connect'
 };
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  urlbase: '',
+  URL_BFF_BASE: 'https://vs-as-appsalud-back-qa.azurewebsites.net',
+  X_IBM_CLIENT_ID: '', /* REVISAR CUÁL CORRESPONDE A QA */
+  CLIENT_ID: 'vs-web-salud',
   URL_SSO: 'https://qa-sso.security.cl:8543/auth/realms/VS-Clients/protocol/openid-connect'
 };
