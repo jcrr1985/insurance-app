9359c73b5310a8cd0ec06efce8c960fded504d2b
Sergio Valentín Asenjo Asencio <sergio.asenjo@ibm.com>
2022-06-03 03:12:11 +0000

    fix(web): arregla calendario y modal de aranceles.
    Se arregla la fecha inicial del calendario para que corresponda a la
    actual.
    
    Se arregla estilo del calendario donde el día seleccionado siempre se
    veía no centrado en el círculo naranjo.
    
    Se arregla estilo del calendario donde la fila donde se encuentra el
    día seleccionado estaba corrido más a la izquierda que el resto de las
    filas.
    
    Se arregla funcionalidad del calendario permitiendo que al seleccionar
    una fecha correcta, el input toma el color verde que corresponde, mientras
    que al escoger una fecha mayor a la actual, el calendario toma el color
    rojo característico de error.
    
    Se añade las validaciones sobre el campo de bonificación y valor cuando
    se está sobre el modal del detalle de aranceles, para que el input de
    bonificación aparezca como erróneo si el monto supera o es igual al del
    input valor.
    
    Se arregla bug de que cuando se selecciona boleta como documento, al
    no aparecer el input de bonificación, el botón de guardar nunca se
    encendía.
    
    Se arregla verificación sobre campo monto solicitado en prestación
    hospitalaria para que si el monto es equivalente a $0 se marque como
    rojo, significando que es un monto erróneo.
    
    Relacionado con: #40389

--- src/app/reembolso/modales/detalle-prestacion/detalle-prestacion.component.ts
+++ src/app/reembolso/modales/detalle-prestacion/detalle-prestacion.component.ts
@@ -154,6 +154,24 @@ export class DetallePrestacionComponent
     let nombrePrestacion = document.querySelector('#nombrePrestacion')?.querySelector('input')?.value
     let numeroSesiones = document.querySelector('#numeroSesiones')?.querySelector('input')?.value
 
+    if (inputBonificacion && inputValor) {
+      const valor =  inputValor.replace(/\D/g,'');
+      const bonificacion = inputBonificacion.replace(/\D/g,'');
+      if (bonificacion >= valor) return true;
+    }
+
+    if (this.esBoletaFactura) {
+      if (inputValor && nombrePrestacion) {
+        if (numeroSesiones) {
+          return false;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    }
+
     if (inputValor &&  nombrePrestacion && inputBonificacion && ( inputBonificacion && Number(inputBonificacion) != 0) ){
       if(numeroSesiones){
       return false
